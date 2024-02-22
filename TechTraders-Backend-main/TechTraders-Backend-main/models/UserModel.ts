269bc28c8f806b26cceddc9da6dfd32b39f76864
Alejandra Eng <129417328+AleDataAnalyst@users.noreply.github.com>
2024-02-22 15:24:26 +0100

    Update UserModel.ts

--- TechTraders-Backend-main/TechTraders-Backend-main/models/UserModel.ts
+++ TechTraders-Backend-main/TechTraders-Backend-main/models/UserModel.ts
@@ -2,32 +2,28 @@ import { connection } from "../database/config";
 
 const UserModel = {
     getAllUsers: async () => {
-        const [result, metadara] = await connection.query('SELECT * FROM users');
+        const [result, metadata] = await connection.query('SELECT * FROM users');
         return result;
     },
     getUser: async (id: string) => {
         const [result, metadata] = await connection.query('SELECT * FROM users WHERE id =?', [id]);
         return result;
     },
-    addUser: async (username: string, telephone: number, email: string, pass: string) => {
-        const [result, metadata] = await connection.query(`INSERT INTO users (username, telephone, email, password) VALUES ('${username}', '${telephone}', '${email}', '${pass}')`);
+    addUser: async (username: string, email: string, pass: string) => {
+        const [result, metadata] = await connection.query(`INSERT INTO users (username, email, pass) VALUES ('${username}', '${email}', '${pass}')`);
         return result;
     },
-    updateUsers: async (id: string, username: string, telephone: number, email: string, pass:string) =>{
-        const [result, metadata] = await connection.query(`UPDATE users SET username = '${username}', telephone = '${telephone}', email = '${email}', password = '${pass}' WHERE id = ${id}`);
+    updateUser: async (id: string, username: string, email: string, pass:string) =>{
+        const [result, metadata] = await connection.query(`UPDATE users SET username = '${username}', email = '${email}', pass = '${pass}' WHERE id = ${id}`);
         return result;
     },
-    deleteUsers: async (id: string) => {
+    deleteUser: async (id: string) => {
         const [result, metadata] = await connection.query(`DELETE FROM users WHERE id = ${id}`);
         return result;
     },
     loginUser : async (username: string, pass: string, email:string) => {
-
-        const [result, metadata] = await connection.query(`SELECT *
-        FROM users
-        WHERE email = '${email}' AND pass = '${pass}'`);
+        const [result, metadata] = await connection.query(`SELECT * FROM users WHERE email = '${email}' AND pass = '${pass}'`);
         return result;
     }
-
 }
 export default UserModel;
