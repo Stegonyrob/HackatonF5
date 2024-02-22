e539282afc57417493cc42ff9961289516686a1d
Stella Maris González Robert <stegonyrob@yahoo.es>
2024-02-22 22:39:52 +0100

    database connect only whith catergories

--- TechTraders-Frontend-main/src/components/card/CardCategories.jsx
+++ TechTraders-Frontend-main/src/components/card/CardCategories.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom'; 
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 function CardCategories() {
   const slides = useSelector(state => state.slides.slides);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/categories');
+        setCategories(response.data);
+      } catch (error) {
+        console.error('Error fetching categories:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div className="card-container" style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
@@ -13,13 +28,13 @@ function CardCategories() {
           <Link to={slide.link} style={{ textDecoration: 'none', color: 'inherit' }}>
             <Card.Img variant="top" src={slide.image} alt={slide.title} style={{ width: '18rem', margin: '1rem', height: '18rem', position: 'relative' }}/>
             <div style={{position: 'absolute', bottom: '130px', left: '100px', width: '60%', color: 'white',  padding: '10px' }}>
-              <h5>{slide.title}</h5>
+              <h5>{categories[index] ? categories[index].title : slide.title}</h5>
             </div>
           </Link>
-          
         </Card>
       ))}
     </div>
-  );}
+  );
+}
 
 export default CardCategories;
